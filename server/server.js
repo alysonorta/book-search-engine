59e766d9a3ac6388aae90b2726084d51c83b5ce9
Alyson Orta <alyson.orta@gmail.com>
2021-10-15 14:48:07 -0400

    deployment issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers} = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
@@ -30,6 +29,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
